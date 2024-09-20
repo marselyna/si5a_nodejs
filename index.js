0478c373f0592f191c4f678dbdfe269ebd149090
test <marselynafitrimawarni@gmail.com>
2024-09-20 11:04:20 +0700

    respon json

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const http = require('http');
 
 //buat server
 const server = http.createServer( (req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('Selamat pagi');
+    // res.writeHead(200, {'Content-Type': 'text/html'});
+    // res.write('Selamat pagi');
+    // res.end();
+
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.write(JSON.stringfy({
+        'message' : 'Selamat Pagi',
+        'status'  : 'succes'
+    }));
     res.end();
 } )
 
