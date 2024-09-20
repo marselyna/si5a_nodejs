df0004e1a5de9deb9d4c216bdfd82f068b9ed4a4
test <marselynafitrimawarni@gmail.com>
2024-09-20 11:16:11 +0700

    list data

--- index.js
+++ index.js
@@ -8,16 +8,28 @@ const server = http.createServer( (req, res) => {
     // res.end();
 
     res.writeHead(200, {'Content-Type': 'application/json'});
+
+    if(req.url === '/dosen'){
+        message = 'List data dosen';
+        data = ['Ahmad', 'Hafiz', 'Widi'];
+    } else if(req.url === '/mahasiswa'){
+        message = 'List data mahasiswa';
+        data = ['Kenny', 'Rizky', 'Akbar'];
+    } else {
+        message = 'Tidak, ditemukan';
+        data = [];
+    }
     res.write(JSON.stringfy({
-        'message' : 'Selamat Pagi',
-        'status'  : 'succes'
+        'message' : message,
+        'data' : data,
+        'status' : 'succes'
     }));
     res.end();
-} )
+});
 
 const port = 3000;
 const host = 'localhost';
 
 server.listen(port, host, () => {
     console.log(`Server berjalan di http://${host}:${port}`);
-} );
+});
